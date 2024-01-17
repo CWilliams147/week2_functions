707de8e158818c70ea9de87be34394bdeea890dd
Cameron Williams <cswilliams112004@gmail.com>
2024-01-17 14:55:50 -0500

    madlib.py

---
+++ madlib.py
@@ -0,0 +1,11 @@
+def madlib(name="John", subject="math"):
+    return f"{name}'s favorite subject is {subject}."
+
+# Examples
+print(madlib("Jenn", "science"))
+# Output: "Jenn's favorite subject is science."
+
+print(madlib("Jeff", "history"))
+# Output: "Jeff's favorite subject is history."
+
+print(madlib())
